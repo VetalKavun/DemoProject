9c00e682d27e14aa4629cd35846e183f52afec87
vitaliikavun <kvital@amazon.com>
2022-01-12 15:10:59 +0100

    added example of bean initialization with JSR250

--- src/main/java/code/spring/configlesson/Main.java
+++ src/main/java/code/spring/configlesson/Main.java
@@ -6,9 +6,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        SimpleBeanImpl simpleBeanImpl1 = (SimpleBeanImpl) context.getBean("initBean1");
-        SimpleBeanImpl simpleBeanImpl2 = (SimpleBeanImpl) context.getBean("initBean2");
-        SimpleBeanImpl simpleBeanImpl3 = (SimpleBeanImpl) context.getBean("initBean3");
+        SimpleBeanJSR250 simpleBeanImpl1 = (SimpleBeanJSR250) context.getBean("initJSR1");
+        SimpleBeanJSR250 simpleBeanImpl2 = (SimpleBeanJSR250) context.getBean("initJSR2");
+        SimpleBeanJSR250 simpleBeanImpl3 = (SimpleBeanJSR250) context.getBean("initJSR3");
         System.out.println(simpleBeanImpl1.toString());
         System.out.println(simpleBeanImpl2.toString());
         System.out.println(simpleBeanImpl3.toString());
